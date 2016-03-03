852f014ab2f593094f044421d9e83dfc6e58c771
over9ooo <myblack.iswhite@gmail.com>
2016-03-03 02:16:27 +0200

    psh

--- target/allure-maven-plugin/src/it/feature-without-version-property/verify.groovy
+++
@@ -1,8 +0,0 @@
-import java.nio.file.Paths
-
-import static ru.yandex.qatools.allure.report.TestHelper.checkReportDirectory
-
-def basedirPath = basedir.absolutePath
-def outputDirectory = Paths.get(basedirPath, 'target', 'site', 'allure-maven-plugin')
-
-checkReportDirectory(outputDirectory, 1)
